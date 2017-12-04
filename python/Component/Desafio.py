6292ae9d785ca7da1f34cf12989262fd9f70101d
Aurelio Gualda <aurelio.10@hotmail.com>
2017-12-04 19:44:58 -0200

    start desafio

---
+++ python/Component/Desafio.py
@@ -0,0 +1,17 @@
+from Component.Interpolar import Interpolar
+
+
+
+
+sair = True
+while(sair):
+    print('###  Escolha uma Opção \t  \t\t\t\t\t\t ### ')
+    print('###  1 para adicionar um ponto na funcao  \t\t ###')
+    print('###  2 para saber os valores dos polinômios  \t ###')
+    print('###  3 para vizualizar  área positva \t\t\t ###')
+    print('###  4 para vizualizar  as raizes \t\t\t\t ###')
+    print('###  5 para limpar a matriz \t\t\t\t\t ###')
+    print('###  0 sair  \t\t\t\t\t ###')
+    option = input()
+
+    sair = False
